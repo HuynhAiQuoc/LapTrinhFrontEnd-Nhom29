b09bba97869eb3fd2587a0ecca720ff360159db0
dukhanh <18130108@st.hcmuaf.edu.vn>
2021-08-10 09:47:52 +0700

    finish chat group
    - get list recent user and group
    - create group
    - join a group
    - chat group

--- src/app/messenger/chat-area/chat-area.component.ts
+++ src/app/messenger/chat-area/chat-area.component.ts
@@ -11,15 +11,16 @@ import * as $ from 'jquery';
   styleUrls: ['./chat-area.component.css']
 })
 export class ChatAreaComponent implements OnInit, AfterViewChecked {
-
-  @ViewChild('scrollBottom', {static: false})private myScrollContainer?: ElementRef;
+  @ViewChild('scrollBottom', { static: false }) private myScrollContainer?: ElementRef;
 
   @Input() listUser: string[] = [];
+  @Input() listGroup: string[] = [];
   @Input() author?: string;
   @Input() username?: string;
   @Input() status?: string;
-  @Input() listMessagePeople: Array<{ type: string, name: string, to: string, mes: string, time: string }> = [];
+  @Input() listMessage: Array<any> = [];
   map?: Map<string, any[]> = new Map();
+  typeChat?: string = '';
 
   constructor(public webSocketService: WebsocketService, private router: Router, private listUserService: ListUserService) {
     const navigation = this.router.getCurrentNavigation();
@@ -29,16 +30,21 @@ export class ChatAreaComponent implements OnInit, AfterViewChecked {
   }
 
   ngOnInit(): void {
-    this.listUserService.getListUser(this.username + '').forEach((item: any) => {
-      if (item.type == 'people') {
-        this.listUser.push(item.user);
-      }
-    })
+
+    this.getListUserRecent();
 
     this.webSocketService.responseServe().forEach(item => {
       if (JSON.parse(item).event === 'SEND_CHAT') {
-        if (JSON.parse(item).data.name == this.author) {
-          this.listMessagePeople.push({ type: 'reply', name: JSON.parse(item).data.name, to: JSON.parse(item).data.to, mes: JSON.parse(item).data.mes, time: this.getTime() })
+        if ((JSON.parse(item).data.name === this.author) && (JSON.parse(item).data.type == 0)) {
+          this.listMessage.push({ type: 'reply', name: JSON.parse(item).data.name, to: JSON.parse(item).data.to, mes: JSON.parse(item).data.mes, time: this.getTime() })
+        }
+      }
+    });
+
+    this.webSocketService.responseServe().forEach(item => {
+      if (JSON.parse(item).event === 'SEND_CHAT') {
+        if ((JSON.parse(item).data.to === this.author) && (JSON.parse(item).data.type == 1)) {
+          this.listMessage.push({ type: 'reply', name: JSON.parse(item).data.name, to: JSON.parse(item).data.to, mes: JSON.parse(item).data.mes, time: this.getTime() })
         }
       }
     });
@@ -46,10 +52,12 @@ export class ChatAreaComponent implements OnInit, AfterViewChecked {
     this.scrollToBottom();
 
   }
+
   ngAfterViewChecked() {
     this.scrollToBottom();
   }
 
+  // auto scroll area chat to bottom 
   scrollToBottom() {
     this.myScrollContainer?.nativeElement.scroll({
       top: this.myScrollContainer.nativeElement.scrollHeight,
@@ -58,34 +66,63 @@ export class ChatAreaComponent implements OnInit, AfterViewChecked {
     })
   }
 
-
-
+  // select a user to connect a contact chat
   selectUser(user: string) {
     this.author = user;
+    this.typeChat = 'people';
     this.map = new Map();
-    this.listMessagePeople = [];
+    this.listMessage = [];
     this.isUserOnline(user);
     this.responseMessagePeople(user);
   }
 
+  // get user chat recent
+  getListUserRecent() {
+    this.listUser = [];
+    var ms = {
+      "action": "onchat",
+      "data": {
+        "event": "GET_USER_LIST",
+      }
+    };
+    this.webSocketService.sendMessage(ms);
+    this.webSocketService.responseServe().forEach(item => {
+      if (JSON.parse(item).event === 'GET_USER_LIST') {
+        JSON.parse(item).data.forEach((res: any) => {
+          if (res.type == 0) {
+            if (!(this.listUser.includes(res.name)) && !(this.username === res.name)) {
+              this.listUser.push(res.name);
+            }
+          } else {
+            if (!(this.listGroup.includes(res.name))) {
+              this.listGroup.push(res.name);
+            }
+          }
+        })
+      }
+    })
+  }
+
+  // submit send message 
   sendMessage(form: NgForm) {
     if (form.value.message.trim() != "") {
-      this.requestMessage(this.author + '', form.value.message.trim());
+      this.requestMessage(this.author + '', form.value.message.trim(), this.typeChat + '');
       let mess = form.value.message.trim();
-      this.listMessagePeople.push({ type: 'sent', name: this.username + '', to: this.author + '', mes: mess, time: this.getTime() })
+      this.listMessage.push({ type: 'sent', name: this.username + '', to: this.author + '', mes: mess, time: this.getTime() })
       form.reset();
     } else {
       return;
     }
   }
 
-  requestMessage(author: string, message: string) {
+  // request send message to people chat
+  requestMessage(author: string, message: string, typeChat: string) {
     var ms = {
       "action": "onchat",
       "data": {
         "event": "SEND_CHAT",
         "data": {
-          "type": "people",
+          "type": typeChat,
           "to": author,
           "mes": message,
         }
@@ -94,20 +131,20 @@ export class ChatAreaComponent implements OnInit, AfterViewChecked {
     this.webSocketService.sendMessage(ms);
   }
 
+  // add a contact chat people and save into localStorage
   addPeopleContact(form: NgForm) {
     var user = form.value.username.trim();
     if (!(user === '')) {
       if (!this.listUser?.includes(user) && !(user === this.username)) {
         this.listUser?.push(user);
-        this.listUserService.addUser(this.username + '', { type: 'people', user: user });
         form.reset();
       }
     } else {
-      // this.listUserService.deleteUser(this.username + '');
     }
   }
 
-  checkStatusUser(user: string) {
+  // request to check status user
+  isUserOnline(user: string) {
     var ms = {
       "action": "onchat",
       "data": {
@@ -118,10 +155,7 @@ export class ChatAreaComponent implements OnInit, AfterViewChecked {
       }
     };
     this.webSocketService.sendMessage(ms);
-  }
 
-  isUserOnline(user: string) {
-    this.checkStatusUser(user);
     this.webSocketService.responseServe().forEach(item => {
       if (JSON.parse(item).event === 'CHECK_USER') {
         if (JSON.parse(item).data.status == true) {
@@ -133,7 +167,7 @@ export class ChatAreaComponent implements OnInit, AfterViewChecked {
     })
   }
 
-
+  // request to get history chat for people
   requestGetMessagePeople(nameAuthor: string, page: number) {
     var ms = {
       "action": "onchat",
@@ -148,22 +182,21 @@ export class ChatAreaComponent implements OnInit, AfterViewChecked {
     this.webSocketService.sendMessage(ms);
   }
 
-
+  // get list history chat for people
   responseMessagePeople(nameAuthor: string): Array<any> {
     let page: number = 1;
     while (page < 10) {
       this.requestGetMessagePeople(nameAuthor, page);
       this.webSocketService.responseServe().forEach(response => {
         if (JSON.parse(response).event == 'GET_PEOPLE_CHAT_MES') {
-
           JSON.parse(response).data.forEach((l: any) => {
             if (!this.map?.has(l.id)) {
               if (l.name === this.username) {
                 this.map?.set(l.id, [{ type: 'sent', name: l.name, to: l.to, mes: l.mes, time: l.createAt }]);
-                this.listMessagePeople.splice(0, 0, { type: 'sent', name: l.name, to: l.to, mes: l.mes, time: l.createAt })
+                this.listMessage.splice(0, 0, { type: 'sent', name: l.name, to: l.to, mes: l.mes, time: l.createAt })
               } else {
                 this.map?.set(l.id, [{ type: 'reply', name: l.name, to: l.to, mes: l.mes, time: l.createAt }]);
-                this.listMessagePeople.splice(0, 0, { type: 'reply', name: l.name, to: l.to, mes: l.mes, time: l.createAt })
+                this.listMessage.splice(0, 0, { type: 'reply', name: l.name, to: l.to, mes: l.mes, time: l.createAt })
               }
             }
           })
@@ -172,9 +205,127 @@ export class ChatAreaComponent implements OnInit, AfterViewChecked {
       })
       page++;
     }
-    return this.listMessagePeople;
+    return this.listMessage;
   }
 
+  // select group chat 
+  selectGroup(groupName: string) {
+    this.author = groupName;
+    this.typeChat = 'room';
+    this.status = '...';
+    this.map = new Map();
+    this.listMessage = [];
+    this.getMessageGroup(groupName);
+    // console.log(this.listMessage);
+  }
+
+  //submit creat a group chat and add name group into list group
+  createGroup(form: NgForm) {
+    var groupName = form.value.nameGroup.trim();
+    if (!(groupName === '')) {
+      this.requestCreateGroup(groupName);
+      this.webSocketService.responseServe().forEach(item => {
+        if (JSON.parse(item).event === 'CREATE_ROOM') {
+          if (JSON.parse(item).status === 'success') {
+            this.listGroup.push(groupName);
+            // this.listUserService.addUser(this.username + '', { type: 'group', user: groupName });
+            form.reset();
+          } else {
+            alert('Group Exist');
+          }
+        }
+      })
+    }
+  }
+
+  //submit join a group chat and add name group into list group
+  joinGroup(form: NgForm) {
+    var groupName = form.value.nameGroup.trim();
+    if (!(groupName === '')) {
+      this.requestJoinGroup(groupName);
+      this.webSocketService.responseServe().forEach(item => {
+        if (JSON.parse(item).event === 'JOIN_ROOM') {
+          if ((JSON.parse(item).status === 'success') && !(this.listGroup.includes(groupName))) {
+            this.listGroup.push(groupName);
+            // this.listUserService.addUser(this.username + '', { type: 'group', user: groupName });
+            form.reset();
+          } else {
+
+          }
+        }
+      })
+    }
+  }
+
+  // request to get message from a group
+  requestGetMessageGroup(groupName: string, page: number) {
+    var ms = {
+      "action": "onchat",
+      "data": {
+        "event": "GET_ROOM_CHAT_MES",
+        "data": {
+          "name": groupName,
+          "page": page
+        }
+      }
+    };
+    this.webSocketService.sendMessage(ms);
+  }
+
+  // get message a group chat
+  getMessageGroup(groupName: string) {
+    let page: number = 1;
+    while (page < 10) {
+      this.requestGetMessageGroup(groupName, page);
+      this.webSocketService.responseServe().forEach(response => {
+        if (JSON.parse(response).event === 'GET_ROOM_CHAT_MES') {
+          JSON.parse(response).data.chatData.forEach((l: any) => {
+            if (!this.map?.has(l.id)) {
+              if (l.name === this.username) {
+                this.map?.set(l.id, [{ type: 'sent', name: l.name, to: l.to, mes: l.mes, time: l.createAt }]);
+                this.listMessage.splice(0, 0, { type: 'sent', name: l.name, to: l.to, mes: l.mes, time: l.createAt })
+              } else {
+                this.map?.set(l.id, [{ type: 'reply', name: l.name, to: l.to, mes: l.mes, time: l.createAt }]);
+                this.listMessage.splice(0, 0, { type: 'reply', name: l.name, to: l.to, mes: l.mes, time: l.createAt })
+              }
+            }
+          })
+        }
+      })
+      page++;
+    }
+  }
+
+
+  // request message to serve to create a new group chat
+  requestCreateGroup(nameGroup: string) {
+    var ms = {
+      "action": "onchat",
+      "data": {
+        "event": "CREATE_ROOM",
+        "data": {
+          "name": nameGroup
+        }
+      }
+    }
+    this.webSocketService.sendMessage(ms);
+  }
+
+  // request message to join a group chat
+  requestJoinGroup(nameGroup: string) {
+    var ms = {
+      "action": "onchat",
+      "data": {
+        "event": "JOIN_ROOM",
+        "data": {
+          "name": nameGroup
+        }
+      }
+    };
+    this.webSocketService.sendMessage(ms);
+  }
+
+  // logout account chat
   onLogout() {
     this.webSocketService.sendMessage({
       "action": "onchat",
@@ -185,6 +336,7 @@ export class ChatAreaComponent implements OnInit, AfterViewChecked {
     this.router.navigateByUrl("/login");
   }
 
+  // get time when send and receive a new message
   getTime(): string {
     var today = new Date();
     var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();;
@@ -205,7 +357,6 @@ export class ChatAreaComponent implements OnInit, AfterViewChecked {
     $("#peopleChat").css({ 'background-color': '#32465a' });
     $("#groupChat").css({ 'background-color': 'rgb(67, 95, 122)' });
   }
-
 
 }
 
